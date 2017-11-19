74df422b6b2bcf4659341ec2d9be87bfd8c8ad0a
Sebastian Giulietti <32080740+giuliettiseba@users.noreply.github.com>
2017-11-19 16:03:56 -0300

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace BinaryTree
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(886, 476);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(809, 482);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(65, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(56, 483);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "buscarKesimo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "kEsimos";
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 483);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "5";
+            
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(192, 484);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(44, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "10";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(242, 483);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "cantidadElementos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 509);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.HelpButton = true;
+            this.Name = "Main";
+            this.Text = "Binary Tree";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BinaryTree
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private BinaryTree _tree;
+
+        void PaintTree()
+        {
+            if (_tree == null) return;
+            pictureBox1.Image = _tree.Draw();
+        }
+
+        private void armarArbol(int personas)
+        {
+
+            List<int> lista = new List<int>();
+            // 
+            //Random rnd = new Random(); 
+            for (int i = 1; i <= personas; i++)
+            {
+                //lista.Add(rnd.Next(100));
+                lista.Add(i);
+            }
+            _tree = cargarArbolBinario(lista);
+            PaintTree();
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Se crea un nuevo arbol
+            _tree = new BinaryTree();
+            // Armo Arbol
+            armarArbol(63);
+        }
+
+        private Node cargarArbolBinario(Node nodo, List<int> lista, int cantidad)
+        {
+            if (cantidad > 0)
+            {
+                int left, right, middle = cantidad / 2;
+                int valor = lista[middle];
+                nodo = new Node(valor);
+                left = middle;
+                right = cantidad - middle - 1;
+                nodo.Left = cargarArbolBinario(nodo.Left, lista, left);
+                nodo.Right = cargarArbolBinario(nodo.Right, lista.GetRange(middle + 1, lista.Count - middle - 1), right);
+                return nodo;
+            }
+            return null;
+        }
+
+        private BinaryTree cargarArbolBinario(List<int> list)
+        {
+            Node root = cargarArbolBinario(null, list, list.Count);
+            BinaryTree arbol = new BinaryTree();
+            arbol.Add(root);
+            return arbol;
+        }
+
+
+        public static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            // Find the correct image codec
+            return codecs.FirstOrDefault(t => t.MimeType == mimeType);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            const double quality = 1;
+            var d = new SaveFileDialog { Filter = @"jpeg files|*.jpg" };
+            try
+            {
+                if (d.ShowDialog() != DialogResult.OK)
+                    return;
+                var bmp = pictureBox1.Image;
+                var qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality,
+                                                                     (long)(quality * 75));
+                // Jpeg image codec
+                var jpegCodec = GetEncoderInfo("image/jpeg");
+                if (jpegCodec == null)
+                    return;
+                var encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = qualityParam;
+                bmp.Save(d.FileName, jpegCodec, encoderParams);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int result))
+            {
+                Node aux = _tree.BuscarKesimo(_tree.RootNode, (Int32.Parse(textBox1.Text)));
+                label1.Text = aux.Value.ToString();
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox2.Text, out int result))
+            {
+                armarArbol(Int32.Parse(textBox2.Text));
+            }
+
+
+
+        }
+
+    }
+}
